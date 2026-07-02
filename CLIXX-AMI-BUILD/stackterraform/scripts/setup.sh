9a71f666d154f01afc288eedb977395f3109083f
Enoch Ayanwale <enochayanwale@outlook.com>
2026-07-02 00:20:24 -0400

    fix: several issues within setup script

--- CLIXX-AMI-BUILD/stackterraform/scripts/setup.sh
+++ CLIXX-AMI-BUILD/stackterraform/scripts/setup.sh
@@ -1,39 +1,32 @@
-##Install the needed packages and enable the services(MariaDb, Apache)
+#!/bin/bash
+## Install the needed packages and enable the services(MariaDb, Apache)
 sudo yum update -y
 sudo yum install git -y
 sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 sudo yum install -y httpd mariadb-server
 sudo yum install -y php-gd php-mbstring php-xml php-mysqlnd nfs-utils git
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo systemctl is-enabled httpd
 
-##Add ec2-user to Apache group and grant permissions to /var/www
-sudo usermod -a -G apache ec2-user
-sudo chown -R ec2-user:apache /var/www
-sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
-cd /var/www/html
-
+## Modify Apache configuration safely
 sudo sed -i '151s/None/All/' /etc/httpd/conf/httpd.conf
 
-##Grant file ownership of /var/www & its contents to apache user
-sudo chown -R apache /var/www
-
-##Grant group ownership of /var/www & contents to apache group
-sudo chgrp -R apache /var/www
-
-##Change directory permissions of /var/www & its subdir to add group write 
-sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
-
-##Recursively change file permission of /var/www & subdir to add group write perm
-sudo find /var/www -type f -exec sudo chmod 0664 {} \;
-
-##Restart Apache
-sudo systemctl restart httpd
-sudo service httpd restart
-
-##Enable httpd
-sudo systemctl enable httpd
+## Configure Keepalive settings
 sudo /sbin/sysctl -w net.ipv4.tcp_keepalive_time=200 net.ipv4.tcp_keepalive_intvl=200 net.ipv4.tcp_keepalive_probes=5
+
+## Add ec2-user to Apache group
+sudo usermod -a -G apache ec2-user
+
+## Download and install WP-CLI globally using sudo to avoid permission blocks
+cd /tmp
+sudo curl -O https://githubusercontent.com
+sudo chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+## Grant final ownership and permissions to /var/www for Apache and ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www
+sudo find /var/www -type d -exec chmod 2775 {} \;
+sudo find /var/www -type f -exec chmod 0664 {} \;
+
+## Enable and Restart Apache
+sudo systemctl enable httpd
+sudo systemctl restart httpd
